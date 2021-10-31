52c98e56b665eea391b66f4f4f078d4abc67fe1f
JordisonMatthew <jordison.m@hotmail.com>
2021-10-31 12:57:27 -0600

    Deployment adjustments of part 3

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 // returns the users table data without password
 const getAllUsers = async () => {
